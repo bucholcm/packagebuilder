af9e7736d03c413a523bcc6cc6288a068247852c
Maciej Bucholc <maciek.bucholc@gmail.com>
2015-04-25 00:18:21 +0200

    added unicode compatibility for Component table

--- buildpackage/models.py
+++ buildpackage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
 class Package(models.Model):
@@ -21,6 +22,7 @@ class ComponentType(models.Model):
         return self.component_set.order_by('name')
 
 
+@python_2_unicode_compatible
 class Component(models.Model):
     component_type = models.ForeignKey(ComponentType)
     name = models.CharField(max_length=255)
